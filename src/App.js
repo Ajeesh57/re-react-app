190ca01055d80e2f0e17278bacc91a797577b909
Ajeesh57 <35171015+Ajeesh57@users.noreply.github.com>
2020-12-03 16:30:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	render (){
+		return (
+			<div className="App">
+				<div className="limited-width">
+					<div className="text-first-container row">
+						<div className="col-md-6 what-xbank">
+							<div className="digital-text">
+								<div className="first-line color-nb">Virtusa's</div>
+								<div className="first-line color-nb">Resume Engine</div><br/>
+								<div className="second-line">Our focus is to automate the search of</div>
+								<div className="second-line">resumes matching the job description and</div>
+								<div className="second-line">other features of that requirement</div>
+							</div><br/>
+							<div className="digital-text">
+								<div className="second-line color-nb">Please enter user name to start the chat</div>
+									<form name="contactDetails" id="contactDetails">
+										<div className="form-group">
+											<label for="firstName"></label>
+											<input type="text" className="form-control1" placeholder="Enter the user name" id="firstName" name="firstName" />
+										</div>
+										<button type="submit" className="btn btn-primary" id="startChat">Start Chat</button>
+									</form>
+								</div>
+							</div>
+						<div className="col-md-6 why-xbank">
+							<div className="digital-text" style={{right:'40%', left:'0'}}>
+							  <div className="first-line color-nb">Chat Window</div>
+							</div>
+							<section className="section-chat" id="section-chat" style={{paddingLeft:'20%', paddingRight:'20%'}}>
+								<div id="root1"></div>
+							</section>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+  
+	}
 }
 
 export default App;
+
